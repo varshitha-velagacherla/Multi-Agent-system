384044e8a1b37bb5300616fe183313fdf4613c00
varshitha392 <varshitha908@gmail.com>
2026-05-31 10:20:21 +0530

    initial commit

---
+++ agents.py
@@ -0,0 +1,80 @@
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search, scrape_url
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+##model setup
+llm = ChatOllama(
+    model="qwen2.5:1.5b",
+    temperature=0
+)
+
+##1st agent
+def built_search_agent():
+    return create_agent(
+        model=llm,
+        tools=[web_search]
+    )
+
+##2 agent
+def built_reader_agent():
+    return create_agent(
+        model=llm,
+        tools=[scrape_url]
+    )
+
+
+##Writer Chain
+
+writer_prompt=ChatPromptTemplate.from_messages([
+    ("system", "You are an expert research writer. Write clear, structured and insightful reports."),
+    ("human", """Write a detailed research report on the topic below.
+
+    Topic: {topic}
+
+    Research Gathered:
+    {research}
+
+    Structure the report as:
+    - Introduction
+    - Key Findings (minimum 3 well-explained points)
+    - Conclusion
+    - Sources (list all URLs found in the research)
+
+    Be detailed, factual and professional."""),
+
+
+])
+
+writer_chain=writer_prompt | llm | StrOutputParser()
+
+##Critic chain
+critic_prompt= ChatPromptTemplate.from_messages([
+    ("system", "You are a sharp and constructive research critic. Be honest and specific."),
+    ("human", """Review the research report below and evaluate it strictly.
+
+Report:
+{report}
+
+Respond in this exact format:
+
+Score: X/10
+
+Strengths:
+- ...
+- ...
+
+Areas to Improve:
+- ...
+- ...
+
+One line verdict:
+..."""),
+])
+
+critic_chain = critic_prompt | llm | StrOutputParser()
